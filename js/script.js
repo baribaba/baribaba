7b702b4350d8158ac54518ec611331b1591881fe
baribaba <baribaba99@gmail.com>
2025-08-09 22:08:04 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('searchBtn').addEventListener('click', function() {
-    let city = document.getElementById('city').value;
-    let query = document.getElementById('searchInput').value.trim();
-    if(query){
-        window.location.href = `search.html?city=${city}&q=${encodeURIComponent(query)}`;
-    } else {
-        alert("Please enter a search term!");
-    }
-});
