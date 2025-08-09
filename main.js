1bdbdf4d34e6366e634d8ccd4ebd4455709965ff
baribaba <baribaba99@gmail.com>
2025-08-10 00:27:09 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// client/js/main.js
-document.getElementById("homeSearch").addEventListener("click", function(){
-  const city = document.getElementById("homeCity").value;
-  const q = document.getElementById("homeQuery").value.trim();
-  const params = new URLSearchParams();
-  if (city) params.set("city", city);
-  if (q) params.set("q", q);
-  window.location.href = "/search.html?" + params.toString();
-});
